78c7ed46a61bb8cb08cb4f27ff289dc2647b830e
Stama Eleazar <eleoatam10@gmail.com>
2019-11-19 20:48:49 -0600

    monojs intsalled

---
+++ public/app.js
@@ -0,0 +1,27 @@
+
+
+
+$('.btno').on('click', function () {
+
+    console.log('btno clicked')
+
+
+
+
+    $.getJSON('/all', function (data) {
+
+        for (var i = 0; i < data.length; i++) {
+
+            console.log(data[i].header);
+            console.log(data[i].link);
+
+            $('#result').append('<tr><th>' + data[i].header + '</th>' +
+                '<th>' + data[i].link + '</th></tr>')
+        }
+
+
+
+    });
+
+
+});
---
+++ server.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const mongojs = require('mongojs');
+
+
+const app = express()
+
+var db = mongojs('scropbd', ['headlines'])
+
+db.on('error', function (err) {
+    console.log('database error', err)
+})
+
+
+app.use(express.json())
+// app.use(express.urlencoded())
+
+app.use(express.static('public')); 
+
+app.get('/', function (req, res) {
+    res.send('at home')
+})
+
+
+
+app.get('/add', function (req, res) {
+    // res.send('Hello World')
+
+    // Make a request for a user with a given ID
+    axios.get('https://www.nhl.com/')
+        .then(function (response) {
+            // handle success
+            // console.log(response);
+
+            const $ = cheerio.load(response.data)
+
+            var headliner = [];
+
+            $('li.related-links__item').each(function (index, element) {
+
+                var header = $(element).find('a').text();
+                var link = $(element).find('a').attr('href');
+
+
+                headliner.push({
+                    header: header,
+                    link: link
+                })             
+
+
+            })
+
+            console.log(headliner)
+
+            
+
+            for (var i =0; i< headliner.length; i++){
+
+
+                
+                db.headlines.save({
+                
+                    header: headliner[i].header,
+                    link: headliner[i].link
+
+                
+                })
+
+
+            }
+
+            res.json(headliner)
+
+           
+
+
+            
+
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+
+
+
+
+})
+
+
+app.get('/all', function(req, res){
+
+    db.headlines.find({}, function(err, data){
+        if(err){
+            console.log(err)
+        }else{
+            
+            console.log(data)
+            res.json(data)
+        }
+    })
+})
+
+app.listen(3000)
+
